1e299fd803c7083934129860609e174e01f37ce3
rek2gnulinux <rek2gnulinux@gmail.com>
2012-08-06 22:27:41 -0700

    ohh I just found out about a ruby nmap parser.. playing with it

---
+++ scan.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+
+require 'nmap/program'
+
+NETWORK = ARGV[0]
+
+Nmap::Program.scan do |nmap|
+    print "Scanning #{NETWORK}..."
+    #nmap.sudo = true
+
+    nmap.syn_scan = true
+    nmap.service_scan = true
+    nmap.os_fingerprint = true
+    nmap.xml = "#{NETWORK}.xml"
+    nmap.verbose = true
+
+    nmap.ports = [20,21,22,23,25,80,110,443,512,522,8080,8081,1080]
+    nmap.targets = "#{NETWORK}"
+end
+
