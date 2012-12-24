d58774905644f5a7e8f0ea4a3ef61a3f99a6c2e1
rek2 <rek2gnulinux@gmail.com>
2012-12-23 19:54:43 -0800

    getting into python wrote a small linux shadow passwd cracker

---
+++ crack.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# always wanted to write my own passwd cracker this uses the modern GNU/Linux
+# sha512 hashing with salt, this may not be usefull when cracking in the real world
+# but can be usefull if you dont have your tools with you and you just have python.
+ 
+
+import base64
+import crypt
+
+def testPass(salt, cryptPass):
+  dictFile = open('/home/rek2/dictionary.txt','r')
+  
+  for word in dictFile.readlines():
+    word = word.strip('\n')
+    output = crypt.crypt(word, '$6$%s$' % salt)
+    dicPasswd = output.split('$')[-1]
+    if (dicPasswd == cryptPass):
+      print "[+] Password Found: "+word+"\n"
+      return
+    print "[+] Password Not Found.\n"
+    return
+
+def main():
+  passFile = open('/home/rek2/shadow')
+  for line in passFile.readlines():
+    if ":" in line:
+      user = line.split(':')[0]
+      cryptPass = line.split(':')[1].strip(' ')
+      prep = cryptPass.split('$6$')[1]
+      salt = prep.split('$')[0]
+      passwd = prep.split('$')[1]
+      print "[*] Cracking Password For: "+user
+      testPass(salt, passwd)
+if __name__ == "__main__":
+  main()
