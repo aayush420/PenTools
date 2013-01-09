5c051a14563a9d71bfd2ecd3203aea147b4f32ff
rek2 <rek2gnulinux@gmail.com>
2013-01-08 21:12:00 -0800

    reset old simplePortScan

--- simplePortScan.py
+++ simplePortScan.py
@@ -3,39 +3,37 @@ from socket import *
 from threading import *
 
 screenLock = Semaphore(value=1)
-
 def connScan(tgtHost, tgtPort):
     try:	    
         connSkt = socket(AF_INET, SOCK_STREAM)
-	connSkt.connect((tgtHost, tgtPort))
-	connSkt.send('MeoW\r\n')
-	results = connSkt.recv(100)
-	screenlock.acquire()
-	print '[+]%d/tcp open'% tgtPort
-	print '[+] ' + str(results)
+        connSkt.connect((tgtHost, tgtPort))
+        connSkt.send('MeoW\r\n')
+        results = connSkt.recv(100)
+        screenlock.acquire()
+        print('[+]%d/tcp open'% tgtPort)
+        print('[+] ' + str(results))
     except:
-	screenLock.acquire()
-	print '[-]%d/tcp closed'% tgtPort
+        screenLock.acquire()
+        print('[-]%d/tcp closed'% tgtPort)
     finally:
-	screenLock.release()
-	connSkt.close()
+        screenLock.release()
+        connSkt.close()
 
 def portScan(tgtHost, tgtPorts):
     try:
-	tgtIP = gethostbyname(tgtHost)
+        tgtIP = gethostbyname(tgtHost)
     except:
-	print "[-] Cannot resolve '%s': Unknown host" %tgtHost
-	return
+        print("[-] Cannot resolve '%s': Unknown host" %tgtHost)
+        return
     try:
-	tgtName = gethostbyaddr(tgtIP)
-	print '\n[+] Scan results for: ' + tgtName[0]
+        tgtName = gethostbyaddr(tgtIP)
+        print('\n[+] Scan results for: ' + tgtName[0])
     except:
-	print '\n[-] Scan Results for: ' + tgtIP
+        print('\n[-] Scan Results for: ' + tgtIP)
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
-	t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
-	t.start()
-
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
 
 def main():
 
@@ -46,10 +44,9 @@ def main():
     tgtHost = options.tgtHost
     tgtPorts = str(options.tgtPort).split(',')
     if (tgtHost == None) | (tgtPorts == None):
-        print parser.usage
-	exit(0)
+        print(parser.usage)
+        exit(0)
     portScan(tgtHost, tgtPorts)
 
 if __name__ == '__main__':
     main()
-
